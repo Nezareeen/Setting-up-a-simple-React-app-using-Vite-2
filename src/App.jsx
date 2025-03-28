5634b353fb7cbfed57181cc9ef7497381c04aef6
Nezareeen <nezareeen@gmail.com>
2025-03-28 13:31:23 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Neo Seelam</h1>      
+      <h2>Github Username: Nezareeen</h2>
+      <h3>Squad: 89</h3>
+      <h4>Batch: Class of 2028</h4>
+      <h5>Campus: Godavari Global University</h5>
+    </>
+  )
+}
+
+export default App
